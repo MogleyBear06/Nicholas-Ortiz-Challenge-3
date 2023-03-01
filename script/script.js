a36f01e8fefe8a16df49f300db1089a5ba15a1b9
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-03-01 12:42:14 -0600

    prompts added for password specifics

--- script/script.js
+++ script/script.js
@@ -7,11 +7,24 @@ const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numericChars = "0123456789";
 const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-// Event listn.- this is tiggered when user selects 'generate password'
+//  Event listn.- this is tiggered when user selects 'generate password'
 generateBtn.addEventListener("click", function () {
 
 let passwordLength = parseInt(prompt("Enter password length- must be between 8 and 128 characters:"));
 while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
 passwordLength = parseInt(prompt("Enter password length- must between 8 and 128 characters:"));
 }
+// Prompts
+let includeLowercase = confirm("Would you like lowercase characters?");
+let includeUppercase = confirm("Would you like uppercase characters?");
+let includeNumeric = confirm("Would you like numeric characters?");
+let includeSpecial = confirm("Would you like special characters?");
+while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+alert("Please select at least one character type.");
+includeLowercase = confirm("Would you like lowercase characters?");
+includeUppercase = confirm("Would you like uppercase characters?");
+includeNumeric = confirm("Would you like numeric characters?");
+includeSpecial = confirm("Would you like special characters?");
 }
+})
+
