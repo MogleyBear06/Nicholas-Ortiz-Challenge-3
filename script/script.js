6b92b99d96320486ff3c53d2666f72b7967876f0
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-03-01 12:32:50 -0600

    added vars, eventlist

--- script/script.js
+++ script/script.js
@@ -1,14 +1,17 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//  Create variable that points to specific element in HTML
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//  Create variables that will be used to generate password
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numericChars = "0123456789";
+const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-  passwordText.value = password;
+// Event listn.- this is tiggered when user selects 'generate password'
+generateBtn.addEventListener("click", function () {
 
+let passwordLength = parseInt(prompt("Enter password length- must be between 8 and 128 characters:"));
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+passwordLength = parseInt(prompt("Enter password length- must between 8 and 128 characters:"));
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
